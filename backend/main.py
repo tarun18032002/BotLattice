706db6a67dbeaef17c564f4040ee3015308beb58
Tarunkumar Thippeswamy <tarunkumar.thippeswamy@amelia.com>
2026-03-14 22:30:18 +0530

    fix: import issue and structure folders/files

--- backend/main.py
+++ backend/main.py
@@ -63,11 +63,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 from fastapi import FastAPI
-from api.routes_ingest import router as ingest_router
-from api.routes_query import router as query_router
-from api.websocket_chat import router as ws_router
+from src.api.routes_ingest import router as ingest_router
+from src.api.routes_query import router as query_router
+from src.api.websocket_chat import router as ws_router
 from fastapi.middleware.cors import CORSMiddleware
-import pipeline.config.settings
+import src.pipeline.config.settings
 
 app = FastAPI(title="RAG Chatbot API")
 app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173"], allow_credentials=True)
